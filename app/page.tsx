9e497194d5709fc23efbf70864099233b6e30058
Angel <villaangel305@gmail.com>
2026-09-24 08:47:18 -0600

    navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import Hero from '@/components/hero';
+import Navbar from '@/components/navbar';
 import '@/styles/index.css';
 
 export default function Index() {
   return (
+    <>
+    <Navbar/>
     <main>
       <Hero />
     </main>
+    </>
   );
 }
---
+++ components/navbar.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import Button from "@/components/ui/Button";
+import '@/styles/navbar.css';
+
+export default function Navbar() {
+    return(
+        <>
+            <header>
+                <nav>
+                    <div className="left-nav">
+                        <h1 className="title">Dentiq</h1>
+
+                    </div>
+                    <div className="right-nav">
+                        <div className="nav-link">
+                            <ul>
+                                <li><a href="#servicios">Servicios</a></li>
+                                <li><a href="#como-funciona">Cómo funciona</a></li>
+                                <li><a href="#testimonios">Testimonios</a></li>
+                                <li><a href="#contacto">Contacto</a></li>
+                            </ul>
+                        </div>
+                        <div className="options-nav">
+                            <Button className="btn-primary-nav" textBtn="Agendar"/>
+                        </div>
+                    </div>
+                </nav>
+            </header>
+        </>
+    );
+}
